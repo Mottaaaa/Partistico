7fa1ef6dbf222a0bed91c2709f8312a2327e4ec7
Mottaaaa <pedro10mota@msn.com>
2018-11-28 09:44:10 +0000

    teste branch

--- app/src/main/java/mlcl/partistico/MainActivity.java
+++ app/src/main/java/mlcl/partistico/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Teste do branch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
