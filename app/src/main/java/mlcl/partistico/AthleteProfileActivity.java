3ca288d5ff5fa1097d304188b08eb5fa56dba71e
Mottaaaa <pedro10mota@msn.com>
2018-11-28 10:33:24 +0000

    AthleteProfileActivity + cores

---
+++ app/src/main/java/mlcl/partistico/AthleteProfileActivity.java
@@ -0,0 +1,13 @@
+package mlcl.partistico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AthleteProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_athlete_profile);
+    }
+}
