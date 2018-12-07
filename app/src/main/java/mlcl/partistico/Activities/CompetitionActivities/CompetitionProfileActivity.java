8476508a8d97014eb38725f7b83b888ec47d07bc
Rick2323 <Ricardo.C.Professional@hotmail.com>
2018-12-07 11:39:49 +0000

    o hitler voltou

--- app/src/main/java/mlcl/partistico/Activities/CompetitionActivities/CompetitionProfileActivity.java
+++ app/src/main/java/mlcl/partistico/Activities/CompetitionActivities/CompetitionProfileActivity.java
@@ -133,7 +133,21 @@ public class CompetitionProfileActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            Fragment fragment = null;
+
+            switch (position) {
+                case 0:
+                    fragment = new CompetitionProfileGeneralData();
+                    break;
+                case 1:
+                    fragment = new CompetitionProfileGeneralData();
+                    break;
+                case 2:
+                    fragment = new CompetitionProfileGeneralData();
+                    break;
+            }
+
+            return fragment;
         }
 
         @Override
