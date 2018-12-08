b17123110815b53921073ddc638ac544dc07da9d
Mottaaaa <pedro10mota@msn.com>
2018-12-08 02:21:43 +0000

    Inicio do Layout da lista de Aquecimentos

---
+++ app/src/main/java/mlcl/partistico/Activities/WarmupActivities/WarmupListActivity.java
@@ -0,0 +1,15 @@
+package mlcl.partistico.Activities.WarmupActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mlcl.partistico.R;
+
+public class WarmupListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warmup_list);
+    }
+}
