2e56a71c2b38ed5f0c3c6c412fa36bd2666ba439
Rick2323 <Ricardo.C.Professional@hotmail.com>
2018-12-08 21:23:38 +0000

    exercise work in progress

--- app/src/main/java/mlcl/partistico/Activities/ExerciseActivities/ExerciseProfileActivity.java
+++ app/src/main/java/mlcl/partistico/Activities/ExerciseActivities/ExerciseProfileActivity.java
@@ -8,19 +8,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import mlcl.partistico.R;
 
 public class ExerciseProfileActivity extends AppCompatActivity implements SensorEventListener {
 
-    SensorManager sensorManager;
+    private SensorManager sensorManager;
     private Sensor sensor;
-    TextView outputX;
-    TextView outputY;
-    TextView outputZ;
+    private TextView outputX;
+    private TextView outputY;
+    private TextView outputZ;
 
-    private static final float NS2S = 1.0f / 1000000000.0f;
-    private final float[] deltaRotationVector = new float[4];
-    private float timestamp;
+    private LineGraphSeries<DataPoint> series;
+
+    private GraphView graph;
+    private int index = 1;
+    private int range = 100;
+    private final int RANGE_INCREMENT = 100;
+    private List<DataPoint> dataPoints = null;
 
 
     @Override
@@ -34,6 +46,38 @@ public class ExerciseProfileActivity extends AppCompatActivity implements Sensor
         outputX = (TextView) findViewById(R.id.textViewX);
         outputY = (TextView) findViewById(R.id.textViewY);
         outputZ = (TextView) findViewById(R.id.textViewZ);
+
+        graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<>();
+
+        graph.getViewport().setScrollable(true);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(50);
+
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(180);
+
+        // use static labels for horizontal and vertical labels
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setVerticalLabels(new String[] {"Good", "Middle", "Bad", "Middle", "Good"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        graph.addSeries(series);
+
+        dataPoints = new ArrayList<>();
+
+        //mSeries1 = new LineGraphSeries<>(generateData());
+
+        /*GraphView graph2 = (GraphView) rootView.findViewById(R.id.graph2);
+        mSeries2 = new LineGraphSeries<>();
+        graph2.addSeries(mSeries2);
+        graph2.getViewport().setXAxisBoundsManual(true);
+        graph2.getViewport().setMinX(0);
+        graph2.getViewport().setMaxX(40);*/
+
     }
 
     @Override
@@ -64,19 +108,49 @@ public class ExerciseProfileActivity extends AppCompatActivity implements Sensor
         //inclination can be calculated as
         int inclination = (int) Math.round(Math.toDegrees(Math.acos(g[2])));
 
-        if (inclination < 25 || inclination > 155)
-        {
+        if (inclination < 25 || inclination > 155) {
             // device is flat
             outputX.setText("Flat");
-        }
-        else
-        {
+        } else {
             // device is not flat
             outputX.setText("Not Flat");
         }
+
+        //if(index < range) {
+        //dataPoints.add((new DataPoint(index, inclination)));
+        //updateGraph();
+        // range += RANGE_INCREMENT;
+        //}
+
+        series.appendData((new DataPoint(index, inclination)), true, 1000);
+        graph.onDataChanged(false, false);
+        index++;
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void updateGraph() {
+
+        //DataPoint[] dataPointsArr = new DataPoint[dataPoints.size()]; // declare an array of DataPoint objects with the same size as your list
+
+        //for (int i = 0; i < dataPoints.size(); i++) {
+        // add new DataPoint object to the array for each of your list entries
+        //dataPointsArr[i] = dataPoints.get(i); // not sure but I think the second argument should be of type double
+        //}
+
+        DataPoint[] dataPointsArr = ((List<DataPoint>) dataPoints).toArray(new DataPoint[dataPoints.size()]);
+
+        series.resetData(dataPointsArr);
+    }
 }
+
+
+
