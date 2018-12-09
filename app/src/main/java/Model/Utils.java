4c0bccca27fc4f4f8f3cf3cbc6c449323f3dd38c
Mottaaaa <pedro10mota@msn.com>
2018-12-09 21:26:40 +0000

    GOOD

--- app/src/main/java/Model/Utils.java
+++ app/src/main/java/Model/Utils.java
@@ -401,11 +401,13 @@ public class Utils {
         dbAdapter.insertExercise(new BDExercise(1, "Avião"));
         dbAdapter.insertExercise(new BDExercise(2, "Perna ao Peito"));
 
+        /*
         dbAdapter.insertWarmUp(new BDWarmUp("Aquecimento1"));
         dbAdapter.insertWarmUpExercise(new BDWarmUpExercise("Ex1", 1, 1));
         dbAdapter.insertWarmUpExercise(new BDWarmUpExercise("Ex2",0,1));
         dbAdapter.insertWarmUpExercise(new BDWarmUpExercise("Ex3",0,1));
         dbAdapter.insertWarmUpExercise(new BDWarmUpExercise("Ex4",0,1));
+        */
         dbAdapter.close();
     }
 
